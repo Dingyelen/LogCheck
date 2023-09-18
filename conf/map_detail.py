9e5cda2ce0e8afdb787f4fd3c2830910ed4e6308
unknown <dingyanlin_happy@qq.com>
2023-09-18 19:01:04 +0800

    [add] TransDoc

--- conf/map_detail.py
+++ conf/map_detail.py
@@ -4,6 +4,3 @@ DOW2_MAP = {'PayFunel': ['ta_dim.dim_38_0_130723', 'icon_id', 'icon_id@icon_id']
             'Summon': ['ta_dim.dim_38_0_131028', 'summon_id', 'summon_id@summon_id'], 
             'Payment': ['ta_dim.dim_38_0_131279', 'payment_itemid', 'payment_itemid@goods_id']
             }
-
-
-
---
+++ createsql/transdoc.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import openpyxl
+import re
+import os
+
+
+class TransDoc:
+    '''
+    property:
+        common_headers -> list: 公共头
+        common_headers_num -> int: 公共头数量
+        event_name -> list: 所有事件名
+        event_name_col -> int: 事件名所在列
+        event_attr_col -> int: 事件属性开始列
+        event_attr -> dict
+    '''
+    def __init__(self, appname) -> str:
+        '''
+        args:
+            appname:项目名称
+        '''
+        self.appname = appname.upper()
+    
+    def __repr__(self) -> str:
+        return f"TransDoc({self.appname})"
+    
+    @property
+    def common_headers(self):
+        try:
+            workbook = openpyxl.load_workbook(f'./LogCheck/transdoc/{self.appname}.xlsx')
+            worksheet = workbook.active
+            common_headers = [cell.value for cell in worksheet[1] if cell.value is not None]
+        except FileNotFoundError:
+            raise FileNotFoundError('''please add the app's excel transdoc in the transdoc path and capitalize the alphabet!!!''')
+            
+        return common_headers
+    
+    @property
+    def event_name(self):
+        workbook = openpyxl.load_workbook(f'./LogCheck/transdoc/{self.appname}.xlsx')
+        worksheet = workbook.active
+        
+        event_col = None
+        for cell in worksheet[1]:
+            if cell.value == '#event_name':
+                event_col = cell.column
+                break
+        if event_col:
+            event_name = []
+            for cell in worksheet.iter_rows(min_row=4, values_only=True):
+                event_value = cell[event_col-1]
+                if event_value is not None:
+                    event_name.append(event_value)
+        else:
+            raise ValueError('''there is not '#event_name' in transdoc''')
+        
+        return event_name
+    
+    @property 
+    def event_name_col(self):
+        return self.common_headers.index('#event_name')+2
+
+    @property 
+    def common_headers_num(self):
+        return len(self.common_headers)    
+    
+    @property 
+    def event_attr_col(self):
+        return self.common_headers_num+2
+    
+    @property         
+    def event_attr(self):
+        event_name = self.event_name
+        event_name_col = self.event_name_col
+        workbook = openpyxl.load_workbook(f'./LogCheck/transdoc/{self.appname}.xlsx')
+        worksheet = workbook.active
+        event_dict = {}
+        for event in event_name:
+            row_num = 0
+            for row in worksheet.iter_rows(values_only=True):
+                cell_value = row[event_name_col-1]
+                row_num += 1
+                if cell_value == event:
+                    event_dict[event] = row_num
+        return event_dict
+        # attr_dict = {}
+        # for event in event_name:
+        #     for cell in worksheet.iter_rows(values_only=True):
+        #         print(event, cell.value)
+                # if cell.value == event:
+                #    print(cell.row)
+            
+
+        
+if __name__ == '__main__':
+    test = TransDoc('dow2')
+    print(test.event_attr)
+    
+
+
