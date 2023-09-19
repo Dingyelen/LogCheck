51dad54a71c6abfc3a6d46d31649f0519df97b6f
unknown <dingyanlin_happy@qq.com>
2023-09-19 15:15:32 +0800

    [update]map_detail.py model.py

--- conf/map_detail.py
+++ conf/map_detail.py
@@ -1,6 +1,23 @@
+from conf.model import ProjectModel
+
+# map_dict
 DOW2_MAP = {'PayFunel': ['ta_dim.dim_38_0_130723', 'icon_id', 'icon_id@icon_id'], 
             'GoldChage': ['ta_dim.dim_38_0_130727', 'gold_reason', 'gold_reason@gold_reason'], 
             'ItemChange': ['ta_dim.dim_38_0_130879', 'item_id', 'item_id@item_id'], 
             'Summon': ['ta_dim.dim_38_0_131028', 'summon_id', 'summon_id@summon_id'], 
             'Payment': ['ta_dim.dim_38_0_131279', 'payment_itemid', 'payment_itemid@goods_id']
             }
+
+
+# tadb_name
+DOW2_DB = 'ta.v_event_38'
+
+# ta_url
+DOW2_URL = 'http://106.52.105.118:8992/querySql'
+
+# ta_password
+DOW2_PASSWORD = '260aXXAfe1LdM3fGKqFFstRlF7p69YhSYRxyQ3MWCSBzBdonFS9TXT4W9KDo5Bg1'
+
+# MODEL
+DOW2 = ProjectModel(appname='dow2', transdoc='DOW2', map_dict=DOW2_MAP, tadb_name=DOW2_DB, ta_url=DOW2_URL, ta_password=DOW2_PASSWORD)
+
--- conf/model.py
+++ conf/model.py
@@ -1,8 +1,11 @@
 class ProjectModel:
-    def __init__(self, appname: str, tadb_name: str, transdoc: str):
+    def __init__(self, appname: str, transdoc: str, map_dict: dict, tadb_name: str, ta_url: str, ta_password: str):
         self.appname = appname
-        self.tadb_name = tadb_name
         self.transdoc = transdoc
+        self.map_dict = map_dict
+        self.tadb_name = tadb_name
+        self.ta_url = ta_url
+        self.ta_password = ta_password
 
     def __repr__(self):
         return f'{self.appname}_Model'
