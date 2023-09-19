480f797f4966bb8e59d05e67fae5ce1db32c8d76
unknown <dingyanlin_happy@qq.com>
2023-09-19 14:58:08 +0800

    [add] chenk_main.py parse_sheelparams

--- check_main.py
+++ check_main.py
@@ -1,7 +1,16 @@
-# import sys
-from conf.map_detail import *
-def some_func():
-    print(DOW2_MAP)
+from createsql.transdoc import TransDoc
+import sys
+import re
 
-if __name__=="__main__":
-    some_func()
+test = TransDoc('dow2', 'DOW2')
+
+def parse_shellparams(shellinputs):
+    params = shellinputs[1:]
+    kwargs = dict(param[2:].split('=') for param in params if re.match('--[a-zA-Z]+', param) and param.count('=') == 1)
+    others = [param for param in params if param[2:].split('=')[0] not in kwargs]
+    if others:
+        fit_param_ = [f'--{param}=value' for param in others]
+        fit_param = '  '.join(fit_param_)
+        raise SyntaxError(f'please check your shellparams 【{others}】, example: 【python mytest.py {fit_param}】')
+    return kwargs
+
