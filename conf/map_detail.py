41cc24893e479aa274946f9c8853f59362e58c77
Dingyelen <dingyanlin_happy@qq.com>
2023-09-20 14:43:10 +0800

    [add] conf.map_detail 添加海上帝国、dow配置

--- conf/map_detail.py
+++ conf/map_detail.py
@@ -8,18 +8,46 @@ DOW2_MAP = {'PayFunel': ['ta_dim.dim_38_0_130723', 'icon_id', 'icon_id@icon_id']
             'Payment': ['ta_dim.dim_38_0_131279', 'payment_itemid', 'payment_itemid@goods_id']
             }
 
+DOW_MAP = {'AddGold': ['ta_dim.dim_9_0_95031', 'itemid', 'itemid@gold_name'], 
+           'CostGold': ['ta_dim.dim_9_0_95031', 'itemid', 'itemid@gold_name'],
+           'AddItem': ['ta_dim.dim_9_0_124717', 'itemid',  'item_id_detail.item_id@id'], 
+           'CostItem': ['ta_dim.dim_9_0_124717', 'itemid',  'item_id_detail.item_id@id'], 
+           'RecruitCard': ['ta_dim.dim_9_0_100730', 'recruitid', 'recruitid@id'], 
+           'Payment': ['ta_dim.dim_9_0_95498', 'payment_itemid', 'payment_itemid@goodsid']
+           }
+
+SEAEMPIRE_MAP = {'PayFunel': ['ta_dim.dim_38_0_130723', 'icon_id', 'icon_id@icon_id'], 
+                 'CurrencyChange': ['ta_dim.dim_36_0_127730', 'currency_reason', 'item_reason@reason'], 
+                 'ItemChange': ['ta_dim.dim_36_0_127718', 'item_id', 'item_id@item_id'], 
+                 'Summon': ['ta_dim.dim_36_0_128199', 'summon_id', 'summon_id@summon_id'], 
+                 'Payment': ['ta_dim.dim_36_0_128493', 'payment_itemid', 'payment_itemid@goods_id']
+            }
 
 # tadb_name
 DOW2_DB = 'ta.v_event_38'
+DOW_DB = 'ta.v_event_9'
+SEAEMPIRE_DB = 'ta.v_event_36'
 
 # ta_url
-DOW2_URL = 'http://106.52.105.118:8992/querySql'
+URL = 'http://106.52.105.118:8992/querySql'
+DOW2_URL = URL
+DOW_URL = URL
+SEAEMPIRE_URL = URL
+
+
 
 # ta_password
 DOW2_PASSWORD = '260aXXAfe1LdM3fGKqFFstRlF7p69YhSYRxyQ3MWCSBzBdonFS9TXT4W9KDo5Bg1'
+DOW_PASSWORD = '260aXXAfe1LdM3fGKqFFstRlF7p69YhSYRxyQ3MWCSBzBdonFS9TXT4W9KDo5Bg1'
+SEAEMPIRE_PASSWORD = '07F9GuaBxkWQM26k1wsrITat3dwC551RxfxO12syQAL43Ll0NzvJVmBn0yFwMBOj'
 
 # MODEL
 DOW2 = ProjectModel(appname='dow2', transdoc='DOW2', map_dict=DOW2_MAP, tadb_name=DOW2_DB, ta_url=DOW2_URL, ta_password=DOW2_PASSWORD)
+DOW = ProjectModel(appname='dow', transdoc='DOW', map_dict=DOW_MAP, tadb_name=DOW_DB, ta_url=DOW_URL, ta_password=DOW_PASSWORD)
+SEAEMPIRE = ProjectModel(appname='seaempire', transdoc='SEAEMPIRE', map_dict=SEAEMPIRE_MAP, tadb_name=SEAEMPIRE_DB, ta_url=SEAEMPIRE_URL, ta_password=SEAEMPIRE_PASSWORD)
 
 
-PROJECT = {DOW2.appname: DOW2}
+PROJECT = {DOW2.appname: DOW2, 
+           DOW.appname: DOW, 
+           SEAEMPIRE.appname: SEAEMPIRE
+           }
