4be4fe76d851f2b3336000eff1b99e67a5fc98a6
Dingyelen <dingyanlin_happy@qq.com>
2023-09-22 16:49:38 +0800

    [fit]conf/map_detail.py DOW2_MAP 参数修正

--- conf/map_detail.py
+++ conf/map_detail.py
@@ -2,7 +2,7 @@ from conf.model import ProjectModel
 
 # map_dict
 DOW2_MAP = {'PayFunel': ['ta_dim.dim_38_0_130723', 'icon_id', 'icon_id@icon_id'], 
-            'GoldChage': ['ta_dim.dim_38_0_130727', 'gold_reason', 'gold_reason@gold_reason'], 
+            'GoldChange': ['ta_dim.dim_38_0_130833', 'gold_reason', 'gold_reason@gold_reason'], 
             'ItemChange': ['ta_dim.dim_38_0_130879', 'item_id', 'item_id@item_id'], 
             'Summon': ['ta_dim.dim_38_0_131028', 'summon_id', 'summon_id@summon_id'], 
             'Payment': ['ta_dim.dim_38_0_131279', 'payment_itemid', 'payment_itemid@goods_id']
